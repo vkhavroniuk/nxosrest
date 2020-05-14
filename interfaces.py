481074f7ab97344505c55d39aa7dead46b2047ae
Volodymyr Khavroniuk <vkhavroniuk@gmail.com>
2020-05-14 15:45:19 -0700

    modified interfaces.py

--- interfaces.py
+++ interfaces.py
@@ -8,6 +8,7 @@ class RequestsCustomError(Exception):
     ''' Class for Custom Exception'''
     status_code: int = 0
 
+
 @dataclass
 class Switch(object):
     ''' Class for NX-OS Switch node'''
@@ -28,28 +29,41 @@ class Switch(object):
         except requests.exceptions.RequestException as e:
             return RequestsCustomError(999)
         return ret
+
+    def cli_show(self,cmd):
+        data = {'ins_api': {'chunk': '0', 'version': '1.0', 'sid': '1',
+                            'input': 'show ver', 'type': 'cli_show',
+                            'output_format': 'json'}}
+        data['ins_api']['input'] = cmd
+        url = 'https://'+self.mgmtip+'/ins'
+        try:
+            ret = self.s.request('post', url, data=json.dumps(data))
+        except requests.exceptions.RequestException as e:
+            return RequestsCustomError(999)
+
+        if ret.status_code == 200:
+            return json.loads(ret.text)['ins_api']['outputs']['output']['body']
+        
     
 def main():
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-    ip='192.168.123.230'
+    ip='1.1.1.1'
     user='admin'
     password='admin'
-    data = {'ins_api': {'chunk': '0', 'version': '1.0', 'sid': '1', 'input': 'show interface status', 'type': 'cli_show', 'output_format': 'json'}}
-
+   
     nx = Switch(user,password,ip)
     nx.login()
-    r = nx.post(data)
-    if r.status_code == 200:
-        output = json.loads(r.text)['ins_api']['outputs']['output']['body']['TABLE_interface']['ROW_interface']
-        print(output)
-        for i in output:
-#            for key,value in i.items():
-#                print(key,value)
-            print(i['interface'],i['state'],'switchport' if i['vlan']!='routed' else 'routed')
-        
-    else:
-        print('Error Code: ' + str(r.status_code))
+
+    intf_status = nx.cli_show('show interface status')['TABLE_interface']['ROW_interface']
+    for i in intf_status:
+       print(i['interface'],i['state'],'switchport' if i['vlan']!='routed' else 'routed')
+
+
+    cdp = nx.cli_show('show cdp neighbors')['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info']
+    for i in cdp:
+        print(i['intf_id'],i['device_id'])
+
 
 if __name__ == '__main__':
     main()
